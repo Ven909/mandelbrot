286c6ac4f28ecfe98ce5381048b8270caf96f4fd
admin <admin@BCS-Ubuntu-06.lrccd.ad.losrios.edu>
2023-04-25 12:52:46 -0700

    finished build

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -19,18 +19,18 @@ View ComplexPlane::getView()
 }
 void ComplexPlane::zoomIn()
 {
-    m_zoomCount++;
-    float x_size = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
-    float y_size = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
-    m_view.setSize(x_size, y_size);
+   m_zoomCount++;
+   float x_size = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
+   float y_size = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
+   m_view.setSize(x_size, y_size);
 
 }
 void ComplexPlane::zoomOut()
 {
-    m_zoomCount--;
-    float x_size = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
-    float y_size = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
-    m_view.setSize(x_size, y_size);
+   m_zoomCount--;
+   float x_size = BASE_WIDTH * (pow(BASE_ZOOM, m_zoomCount));
+   float y_size = BASE_HEIGHT * m_aspectRatio * (pow(BASE_ZOOM, m_zoomCount));
+   m_view.setSize(x_size, y_size);
 
 }
 void ComplexPlane::setCenter(Vector2f coord)
@@ -43,16 +43,77 @@ void ComplexPlane::setMouseLocation(Vector2f coord)
 }
 void ComplexPlane::loadText(Text& text)
 {
+   stringstream out;
 
+	out << "Mandelbrot Set\n";  //print tittle of game
+   out << "Center:(" << m_view.getCenter().x << "," << m_view.getCenter().y << ")" << endl;  //print the center
+   out << "Cursor:(" << m_mouseLocation.x << "," << m_mouseLocation.y << ")" << endl;        //print the mouse location
+   out << "Left-click to Zoom in" << endl;     //instructions for user on how to use program
+   out << "Right-click to Zoom out" << endl;
+   // Set the text string of the Text object to the stringstream contents
+   text.setString(out.str());
 
+   text.setCharacterSize(22);       //setting text size
+   text.setFillColor(Color::White); //setting text color
+   text.setPosition(50,50);         //setting text position
 }
 size_t ComplexPlane::countIterations(Vector2f coord)
 {
+   complex<double> c(coord.x, coord.y);  //turn pixel's coord into a compex number
+   //initialize to 0
+   complex<double> z(0,0);
+   size_t count = 0;
 
+   //do it until max iterations z surpasses the radius 2
+   while(abs(z) < 2 && count < MAX_ITER)
+   {  
+      z = z*z + c;
+      count++;
+   }
+   return count;
 
 }
 void ComplexPlane::iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b)
 {
+   //creating a color scheme
+   //simple color scheme
+   if(count == MAX_ITER)
+  {
+    r = 0;
+    g = 0;
+    b = 0;
+  }
+  else if(count < 10)
+  {
+    r = 127;
+    g = 0;
+    b = 255;
+  }
+  else if(count < 20)
+  {
+    r = 0;
+    g = 255;
+    b = 0;
+  }
+  else if(count < 30)
+  {   
+    r = 0;
+    g = 0;
+    b = 255;
+  }
+  else if(count < 40)
+  {
+    r = 255;
+    g = 255;
+    b = 0;
+  }
+  else if(count < 50)
+  {
+    r = 255;
+    g = 0;
+    b = 0;
+  }
+
   
 }
 
--- code/main.cpp
+++ code/main.cpp
@@ -12,6 +12,106 @@ using namespace std;
 int main()
 {
    Event event;
-   VideoMode vm(1920, 1080);
+   //set size of window	with respect to the computer widthe and height
+   float width = VideoMode::getDesktopMode().width;
+   float height = VideoMode::getDesktopMode().height;
+   
+   VideoMode vm(width, width);  //set the size
+   // Construct the window
+    RenderWindow window(VideoMode(width, height), "Mandelbrot Set");
+
+   ComplexPlane plane(height /width);  //so that it doesn't get distorted
+
+   //making font and text objects
+   Font font;
+   font.loadFromFile("fonts/KOMIKAP_.ttf");
+
+   Text message;
+   message.setFont(font);
+   message.setCharacterSize(24);
+   message.setPosition(10, 10);
+
+   //construct vertex array to draw a color for each pixel
+   VertexArray vArray(Points, height * width);
+
+   enum State{CALCULATING, DISPLAYING};
+
+   // initialize the state to  CALCULATING
+   State state = CALCULATING;
+
+   while(window.isOpen())  //Handle Input segment 
+   {
+      while (window.pollEvent(event))
+      {
+         if ((event.type == Event::Closed)  || (Keyboard::isKeyPressed(Keyboard::Escape)))
+         {
+            window.close();
+         }
+         //if the user presses mouse.....
+         if(event.type == Event::MouseButtonPressed)
+         {
+            Vector2f pos = window.mapPixelToCoords(Mouse::getPosition(window), plane.getView());
+            //if user clicks right mouse button zoom out
+            if (event.mouseButton.button == Mouse::Right) 
+            {
+               plane.zoomOut();
+               plane.setCenter(pos);  //find the position
+               state = State::CALCULATING;     //generate the new complex plane view
+            } 
+            //else if the user clicks left mosue button zoom in
+            else if (event.mouseButton.button == Mouse::Left) 
+            {
+               plane.zoomIn();
+               plane.setCenter(pos);
+               state = State::CALCULATING;
+            }
+         }
+         //when the mosue moves....
+         if (event.type == Event::MouseMoved) 
+         {
+            //find the Vector2f coordinates in the ComplexPlane view that corresponds to the mouse location
+            Vector2f pos = window.mapPixelToCoords(Mouse::getPosition(window), plane.getView());
+
+            //setMouseLocation on the ComplexPlane object to store this coordinate
+            plane.setMouseLocation(pos);
+         } 
+      }
+      if (state == State::CALCULATING) 
+      {
+         for (auto i = 0; i < height; i++) 
+         {
+            for (auto j = 0; j < width; j++) 
+            {
+               // Set the position of the vertex to the pixel coordinates
+               vArray[j + i * width].position = { (float)j,(float)i };
+
+               // find the coordinate in the ComplexPlane View that corresponds to the screen pixel location at j,i
+               Vector2f coord = window.mapPixelToCoords({j, i}, plane.getView());
+
+               // get and store the number of iterations
+               int iterations = plane.countIterations(coord);
+
+               // store the RGB values for the current pixel 
+               Uint8 r, g, b;
+               //Pass the number of iterations and the RGB variables into iterationsToRGB()
+               plane.iterationsToRGB(iterations, r, g, b);
+
+               // set the vertex color to the corrsponding rgb color
+               vArray[j + i * width].color = {r, g, b};
+            }
+         }
+         // set the state to DISPLAYING
+         state = State::DISPLAYING;
+      }
+
+      // Update the text on the screen
+      plane.loadText(message);
+      //draw everything
+      window.clear();
+      window.draw(vArray);
+      window.draw(message);
+      window.display();
+   
+   }
 }
 
