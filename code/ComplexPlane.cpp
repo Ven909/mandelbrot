a6f006dbcf2d625d56fcb28d27ccd8d5dd6818c7
Ven909 <130593258+Ven909@users.noreply.github.com>
2023-04-26 06:48:15 -0700

    Update some comments

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -9,7 +9,7 @@ using namespace sf;
 ComplexPlane::ComplexPlane(float aspectRatio)
 {
    m_aspectRatio = aspectRatio;
-   m_view.setSize(BASE_WIDTH, -BASE_HEIGHT * m_aspectRatio); //Might need as Vector2f?
+   m_view.setSize(BASE_WIDTH, -BASE_HEIGHT * m_aspectRatio); 
    m_view.setCenter(0.0, 0.0);
    m_zoomCount = 0;
 }
